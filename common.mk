745956efd6fe2730eed2ce291f02397ad52e2514
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    sm8150-common: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- common.mk
+++ common.mk
@@ -391,6 +391,12 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Recorder
 PRODUCT_PACKAGES += \
     OPScreenRecorder
