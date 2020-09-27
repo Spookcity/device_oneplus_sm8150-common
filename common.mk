9130c7455ee45861c067ea08bb5b30c69572ae0a
Rick M <spookcity138@gmail.com>
2020-09-27 12:23:59 +0200

    sm8150-common: Add snap camera to build

--- common.mk
+++ common.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    snap
 
 # CNE
 PRODUCT_PACKAGES += \
