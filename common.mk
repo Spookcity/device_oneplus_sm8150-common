6e5146bd6b5d9def6bfa8bf2ea53b1dda78a1034
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-10-20 07:58:46 +0530

    sm8150-common: Switch to prebuilt nfc stack
    This reverts commit "[DNM] sm8150-common: switch to aosp nfc stack"
    1328a0bad1f18691a30870a1c9c4f93eee855d75.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -363,14 +363,16 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2 \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc@1.0:64 \
+    android.hardware.nfc@1.1:64 \
+    android.hardware.nfc@1.2:64 \
+    android.hardware.secure_element@1.0:64 \
+    android.hardware.secure_element@1.1:64 \
     com.android.nfc_extras \
-    com.gsma.services.nfc \
-    NfcNci \
-    QuickAccessWallet \
-    SecureElement \
-    Tag
+    Tag \
+    vendor.nxp.nxpese@1.0:64 \
+    vendor.nxp.nxpnfc@1.0:64 \
+    QuickAccessWallet
 
 # OMX
 PRODUCT_PACKAGES += \
