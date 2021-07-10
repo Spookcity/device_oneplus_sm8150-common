c046b9110c5d1e8aee5a07ac48fed65309607186
LuK1337 <priv.luk@gmail.com>
2021-07-10 15:24:13 +0200

    sm8150-common: Call oem_qmi_access_func() on RIL init
    This makes us dump NV items on boot which in the end fixes invalid MAC
    address for BT and WLAN.
    
    This change should be reverted as soon as RIL stack is fixed.
    
    Change-Id: I4a2756c91230f43d0c9528d1a685aa7c90723590

--- extract-files.sh
+++ extract-files.sh
@@ -41,6 +41,9 @@ function blob_fixup() {
     vendor/lib/hw/camera.qcom.so | vendor/lib64/hw/camera.qcom.so)
         sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
         ;;
+        vendor/bin/hw/qcrild)
+            "${PATCHELF}" --add-needed libril_shim.so "${2}"
+            ;;
     esac
 }
 
