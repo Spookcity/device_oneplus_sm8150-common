7601a8e23b49e0cd33c45124d95d88d44e014534
Rick M <spookcity138@gmail.com>
2021-12-24 11:52:52 +0100

    sm8150-common: Dont ship any camera app
    Users can install gcam etc of their choice

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := SafetyHubPrebuilt TipsPrebuilt AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt Drive GoogleCamera Maps Photos PrebuiltGmail Videos SecurityHubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := SafetyHubPrebuilt TipsPrebuilt AmbientSensePrebuilt Camera2 CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt Drive GoogleCamera Maps Photos PrebuiltGmail Videos SecurityHubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
