34c0e0fc18fec381ab2561e6e301ce00d195f474
Rick M <spookcity138@gmail.com>
2022-10-25 17:42:54 +0200

    sm8150-common: Dont ship camera

--- common.mk
+++ common.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    GCamGOPrebuilt-V3 \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
